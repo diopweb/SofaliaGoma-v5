4efe60d500d6e6d80925cfcc85d7eaa0939da181
Alioune DIOP (diopweb) <diopweb@gmail.com>
2025-08-03 07:03:33 +0000

    modified:   src/lib/firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,14 +2,17 @@ import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  };
+  apiKey: "AIzaSyAnNBQzriKP4k1F60IeABoq3dqrj5lxmKY",
+  authDomain: "sofalia-goma.firebaseapp.com",
+  projectId: "sofalia-goma",
+  storageBucket: "sofalia-goma.firebasestorage.app",
+  messagingSenderId: "670754253328",
+  appId: "1:670754253328:web:709c3c52dd9b99d130b895",
+  measurementId: "G-VC4QG2XBET"
+};
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
