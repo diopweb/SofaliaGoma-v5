37a57a34a8ba0d2daa2f897a90c2290e72bf0712
Alioune DIOP (diopweb) <diopweb@gmail.com>
2025-08-11 19:54:14 +0000

    new file:   src/lib/firebase-admin.ts 	modified:   src/lib/firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,6 @@
 import { initializeApp, getApp, getApps, FirebaseOptions } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore as getClientFirestore } from 'firebase/firestore';
-import * as admin from 'firebase-admin';
 
 const firebaseConfig: FirebaseOptions = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -17,24 +16,9 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const db = getClientFirestore(app);
 
-// Initialize Firebase Admin for server-side
-if (typeof window === 'undefined' && admin.apps.length === 0) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    }),
-    databaseURL: `https://${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.firebaseio.com`,
-  });
-}
-
 const getDb = () => {
-  if (typeof window === 'undefined') {
-    return admin.firestore();
-  } else {
-    return db;
-  }
+  // This function now only returns the client-side Firestore instance
+  return db;
 };
 
 export { app, auth, db, getDb };
